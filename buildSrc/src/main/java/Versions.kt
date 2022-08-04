347f3c661ffe8f5b2b9ee8889031fec62c17885e
hehua2008 <hegan2010@gmail.com>
2022-08-04 17:29:02 +0800

    Use buildSrc to manage build operations and versions

---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,44 @@
+/**
+ * @author hehua2008
+ * @date 2022/8/4
+ */
+object Versions {
+    // region sdk
+    const val MIN_SDK = 21
+    const val TARGET_SDK = 31
+    const val COMPILE_SDK = 31
+    // endregion
+
+    const val KOTLIN = "1.6.21"
+
+    // region android
+    const val ANDROID_CORE = "1.8.0"
+    const val ANDROID_APPCOMPAT = "1.4.2"
+    const val ANDROID_FRAGMENT = "1.5.1"
+    const val ANDROID_ANNOTATION = "1.4.0"
+    const val ANDROID_CONSTRAINTLAYOUT = "2.1.4"
+    const val ANDROID_LIFECYCLE = "2.5.1"
+    const val ANDROID_WORK = "2.7.1"
+    const val ANDROID_PAGING = "3.1.1"
+    const val ANDROID_SWIPEREFRESHLAYOUT = "1.1.0"
+    // endregion
+
+    // region google
+    const val GOOGLE_MATERIAL = "1.6.1"
+    const val GOOGLE_FLEXBOX = "2.0.1"
+    const val GOOGLE_EXO_PLAYER = "2.18.1"
+    const val GOOGLE_GSON = "2.9.1"
+    // endregion
+
+    // region square
+    const val OKHTTP3 = "4.9.2"
+    const val RETROFIT2 = "2.9.0"
+    const val LEAKCANARY = "2.8.1"
+    // endregion
+
+    // region glide
+    const val GLIDE = "4.13.2"
+    const val GLIDE_TRANSFORMATIONS = "4.3.0"
+    const val GPUIMAGE = "2.1.0"
+    // endregion
+}
