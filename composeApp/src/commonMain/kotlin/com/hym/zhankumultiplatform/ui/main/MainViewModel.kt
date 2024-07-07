2d9b151e1be9f1c9b5fb5f552b0c7bc6bb1551c2
hehua2008 <hegan2010@gmail.com>
2024-07-07 16:58:42 +0800

    Fix pager scroll bugs

--- composeApp/src/commonMain/kotlin/com/hym/zhankumultiplatform/ui/main/MainViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/hym/zhankumultiplatform/ui/main/MainViewModel.kt
@@ -1,15 +1,10 @@
 package com.hym.zhankumultiplatform.ui.main
 
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.hym.zhankumultiplatform.flow.MutableSharedData
 import com.hym.zhankumultiplatform.flow.SharedData
 
 class MainViewModel : ViewModel() {
-    var selectedPage by mutableIntStateOf(0)
-
     private val _word = MutableSharedData<String>("")
     val word: SharedData<String> = _word
 
