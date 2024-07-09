bb4dbc8c534645bafa508695b2db62362f8a50b0
hehua2008 <hegan2010@gmail.com>
2024-07-09 18:47:47 +0800

    Use Ksoup instead Jsoup

--- composeApp/src/commonMain/kotlin/com/hym/zhankumultiplatform/ui/detail/DetailContent.kt
+++ composeApp/src/commonMain/kotlin/com/hym/zhankumultiplatform/ui/detail/DetailContent.kt
@@ -1,6 +1,7 @@
 package com.hym.zhankumultiplatform.ui.detail
 
 import androidx.compose.runtime.Immutable
+import com.fleeksoft.ksoup.Ksoup
 import com.hym.zhankumultiplatform.model.ArticleDetails
 import com.hym.zhankumultiplatform.model.ProductImage
 import com.hym.zhankumultiplatform.model.ProductVideo
@@ -9,7 +10,60 @@ import com.hym.zhankumultiplatform.model.ProductVideo
  * @author hehua2008
  * @date 2022/8/12
  */
-expect fun ArticleDetails.toDetailContent(): List<DetailContent<*>>
+fun ArticleDetails.toDetailContent(): List<DetailContent<*>> {
+    val doc = Ksoup.parse(articledata.memoHtml)
+    val list = mutableListOf<DetailContent<*>>()
+    val body = doc.selectFirst("body") ?: return list
+    var stringId = 0
+    val sb = StringBuilder()
+    body.children().forEach {
+        val img = it.selectFirst("img")
+        if (img == null) {
+            if (it.text().isNotBlank()) {
+                sb.append(it.toString())
+            }
+        } else {
+            if (sb.isNotEmpty()) {
+                list.add(DetailText(data = sb.toString(), id = stringId++))
+                sb.setLength(0)
+            }
+            val imageUrl = img.absUrl("src").let { url ->
+                if (url.isNullOrBlank()) return@forEach
+                url.substringBefore('?').substringBefore('@')
+            }
+            val articleImage = articleImageList.first { articleImage ->
+                articleImage.img.contains(imageUrl, ignoreCase = true)
+            }
+            val productImage = ProductImage(
+                0L,
+                0,
+                imageUrl,
+                "",
+                0,
+                0,
+                "",
+                0,
+                "",
+                "",
+                0,
+                0L,
+                0,
+                0L,
+                "",
+                articleImage.middleImg,
+                imageUrl,
+                articleImage.smallImg,
+                0
+            )
+            list.add(DetailImage(productImage))
+        }
+    }
+    if (sb.isNotEmpty()) {
+        list.add(DetailText(data = sb.toString(), id = stringId++))
+        sb.setLength(0)
+    }
+    return list
+}
 
 @Immutable
 sealed class DetailContent<T>(
